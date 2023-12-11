8d8ee164d96e56f42c6a844865dd4c70ccbf0df8
Grenish rai <ariestheracer@gmail.com>
2023-12-11 12:50:23 +0530

    Fixed and encounter bug

--- src/lexer.js
+++ src/lexer.js
@@ -1,7 +1,34 @@
-const lexer = (input) => {
-  return input
-    .split(/\s+/) // Split on whitespace
-    .filter((token) => token.length > 0);
+const lexer = (code) => {
+  let tokens = [];
+  let currentToken = "";
+  let isString = false;
+
+  for (let char of code) {
+    if (char === " " && !isString) {
+      if (currentToken) {
+        tokens.push(currentToken);
+        currentToken = "";
+      }
+    } else if (char === '"') {
+      if (isString) {
+        currentToken += char;
+        tokens.push(currentToken);
+        currentToken = "";
+      } else {
+        currentToken += char;
+      }
+      isString = !isString;
+    } else {
+      currentToken += char;
+    }
+  }
+
+  if (currentToken) {
+    tokens.push(currentToken);
+  }
+
+  // console.log("Inside Lexer:", tokens);
+  return tokens;
 };
 
 export default lexer;
