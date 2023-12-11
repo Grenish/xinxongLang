6fcda37594ebbebbc41f9eb7637ed569791d6aea
Grenish rai <ariestheracer@gmail.com>
2023-12-11 12:49:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import fs from 'fs';
 import lexer from './src/lexer.js';
-import parser from './src/parser.js';
+import parse from './src/parser.js';
 import XinxongInterpreter from './src/interpreter.js';
 
 const filename = 'example-code.xinxong';
 const code = fs.readFileSync(filename, 'utf-8');
 const tokens = lexer(code);
-const ast = parser(tokens);
+// console.log('Tokens before parsing:', tokens);
+const ast = parse(tokens);
 
 const interpreter = new XinxongInterpreter();
 interpreter.interpret(ast);
+
+
+// console.log('Tokens:', tokens);
+// console.log('AST:', ast);
+// console.log(interpreter);
